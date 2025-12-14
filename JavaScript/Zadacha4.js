45474711544447642513dd26fe7aeb4dbf8c9571
viktorlevskibg-blip <viktorlevskibg@gmail.com>
2025-12-14 22:43:03 +0200

    Add files via upload

--- JavaScript/Zadacha4.js
+++ JavaScript/Zadacha4.js
@@ -1,4 +1,5 @@
-function PrintNumber1(input){
+function PrintNumber1(input)
+{
 let i = 1;
 while (i < 10){
 i++
@@ -15,10 +16,33 @@ PrintNumber1()
 console.log(`------------`)
 function PrintNumber2(input)
 {
-let i = 1;
-while (i < 10){
-i++;
+let i = 10;
+do{
+if (i % 2 == 0){
+    console.log(i * 5)
 }
-
+else {
+    console.log(i)
+}
+i++
+}
+ while (i <= 20)
 }
 PrintNumber2()
+console.log(`------------`)
+function PrintNumber3(input)
+{
+let i = 20;
+while (i > 0)
+    { 
+    {
+        console.log(i)
+    }
+   i--
+}
+}
+PrintNumber3()
+console.log(`------------`)
+
+
+
