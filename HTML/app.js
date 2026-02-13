a3f2123474a5525e6a7ecc2c7ba7dd74de7788d3
viktorlevskibg-blip <viktorlevskibg@gmail.com>
2026-02-13 15:50:16 +0200

    Add files via upload

--- HTML/app.js
+++ HTML/app.js
@@ -1,1 +1,10 @@
-func
+function AddText(){
+    const TextAdd = document.getElementById('TextAdd');
+    const Add = document.getElementById('Add').value;
+    Add.addEventListener('click',(event)=>{
+    console.log(TextAdd.value)
+    const li = document.getElementById('li');
+    list.appendChild(li);
+    })
+    
+}
